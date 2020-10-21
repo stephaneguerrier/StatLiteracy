41b6ff03169eaa150c146001260687614dc26e00
Stéphane Guerrier <stephane.guerrier@unige.ch>
2020-10-21 12:13:20 +0200

    Redo gifs

--- Code for gif/gif_sample_stat.R
+++ Code for gif/gif_sample_stat.R
@@ -10,14 +10,14 @@ n1 = 30
 n2 = 40
 n = n1*n2
 for_a = 0.52
-alpha = 0.05
+alpha = 0.4
 set.seed(16)
 my_sample = sample(1:n)
 
 library("gifski")
 png_path <- file.path(tempdir(), "frame%03d.png")
 png(png_path, units="px", width=1600*1.4, height=1600*1.125, res=300)
-par(ask = FALSE, mar = c(1.75,0.5,0.5,0.5))
+par(ask = FALSE, mar = c(3.25,0.75,0.75,0.75))
 nb = 61
 
 for (k in 1:nb){
@@ -27,19 +27,19 @@ for (k in 1:nb){
   lines(c(27,37), c(34,34))
   ymax1 = for_a*6 + 34
   ymax2 = (1 - for_a)*6 + 34
-  
+
   rect(28, 34, 31, ymax1, col = cols[1])
   rect(33, 34, 36, ymax2, col = cols[2])
-  
+
   text(29.5, 32.6, "N")
   text(34.5, 32.6, "Y")
   rect(29.5 - 1, 32.6 - 1, 29 + 1.5, 32 + 1.5, border = cols[1], lwd = 2)
   rect(4, 35.5, 6, 37, col = cols[1])
   rect(4, 33, 6, 34.5, col = cols[2])
-  
+
   text(15, 35.5/2 + 37/2, paste("Against:  ",100*for_a, "%", sep = ""))
   text(15, 35.5/2 + 37/2 - 2.5, paste("For:       ",100*(1-for_a), "%", sep = ""))
-  
+
   for (i in 1:30){
     for (j in 1:40){
       counter = counter + 1
@@ -50,8 +50,8 @@ for (k in 1:nb){
       }
     }
   }
-  
-  
+
+
   if (k > 1){
     for (l in 1:(k-1)){
       for (h in 1:10){
@@ -65,7 +65,7 @@ for (k in 1:nb){
         points(mod + 1, ypos, col = 1, pch = 16, cex = 0.9)
       }
     }
-    
+
     for (g in 1:counter2){
       if (my_sample[g] <= n*for_a){
         points((g-1) %% (n2/2) + 55, floor((g-1)/(n2/2)) + 1, col = cols[1], pch = 16, cex = 0.9)
@@ -74,26 +74,26 @@ for (k in 1:nb){
         points((g-1) %% (n2/2) + 55, floor((g-1)/(n2/2)) + 1, col = cols[2], pch = 16, cex = 0.9)
       }
     }
-    
+
     delta = 43
     text(15 + delta + 5, 40, paste("Sample (", round(counter2/n*100, 1), "% of pop.)", sep = ""), cex = 1.25)
     lines(c(27,41) + delta, c(34,34))
     ymax1 = (1-counter3/counter2)*6 + 34
     ymax2 = (counter3/counter2)*6 + 34
-    
+
     rect(28 + delta, 34, 31 + delta, ymax1, col = cols[1])
     rect(33 + delta, 34, 36 + delta, ymax2, col = cols[2])
-    
+
     text(29.5 + delta, 32.6, "N")
     text(34.5 + delta, 32.6, "Y")
     text(39.5 + delta, 32.6, "?")
-    
+
     rect(4 + delta -1, 35.5, 6 + delta - 1, 37, col = cols[1])
     rect(4 + delta -1, 33, 6 + delta - 1, 34.5, col = cols[2])
-    
+
     text(15 + delta -1, 35.5/2 + 37/2, paste("Against:  ",round(100*(1 - counter3/counter2),1), "%", sep = ""))
     text(15 + delta -1 , 35.5/2 + 37/2 - 2.5, paste("For:       ",round(100*(counter3/counter2),1), "%", sep = ""))
-    
+
     if (qbeta(1 - alpha/2, counter3 + 1, counter2 - counter3) < 0.5 || qbeta(alpha/2, counter3, counter2 - counter3 + 1) > 0.5){
       if (counter3/counter2 < 0.5){
         rect(29.5 + delta - 1, 32.6 - 1, 29 + delta + 1.5, 32 + 1.5, border = cols[1], lwd = 2)
@@ -108,25 +108,25 @@ for (k in 1:nb){
     }else{
       rect(34.5 + 5 + delta - 1, 32.6 - 1, 34 + 5 + delta + 1.5, 32 + 1.5, border = 1, lwd = 2)
     }
-      
-    
-    
+
+
+
     prop_min = 55 - 10
     prop_max = 74 + 10
     prop_y = -2.5
     delta_y_prop = 0.5
     lines(c(prop_min, prop_max), c(prop_y, prop_y))
     d = prop_max - prop_min
-    
+
     for (u in 0:10){
       lines(c(prop_min, prop_min) + u/10*d, c(prop_y - delta_y_prop,prop_y+delta_y_prop))
       text(prop_min + u/10*d, -5, u/10, cex = 0.9)
     }
-    
+
     lines(c(prop_min,prop_min) + 5/10*d, c(prop_y - 1.5*delta_y_prop,prop_y+1.5*delta_y_prop), lwd = 2)
     text(prop_min, -0.5, "No", cex = 0.9)
     text(prop_min + d, -0.5, "Yes", cex = 0.9)
-    
+
     if (counter3/counter2 >= 0.5){
       points(prop_min + counter3/counter2*d, prop_y, col = cols[2], cex = 2, pch = 16)
       rect(prop_min + d*qbeta(1 - alpha/2, counter3 + 1, counter2 - counter3), prop_y - 1, prop_min + d*qbeta(alpha/2, counter3, counter2 - counter3 + 1), prop_y + 1, col = cols_trans[2], border = "NA")
@@ -134,10 +134,10 @@ for (k in 1:nb){
       points(prop_min + counter3/counter2*d, prop_y, col = cols[1], cex = 2, pch = 16)
       rect(prop_min + d*qbeta(1 - alpha/2, counter3 + 1, counter2 - counter3), prop_y - 1, prop_min + d*qbeta(alpha/2, counter3, counter2 - counter3 + 1), prop_y + 1, col = cols_trans[1], border = "NA")
     }
-    
+
   }
 
-  
+
 }
 
 
@@ -146,7 +146,7 @@ dev.off()
 
 png_files <- sprintf(png_path, 1:nb)
 gif_file <- tempfile(fileext = ".gif")
-gifski(png_files, "GIF/sample95.gif", delay = 0.15, progress = FALSE)
+gifski(png_files, "GIF/sample60.gif", delay = 0.25, progress = FALSE)
 unlink(png_files)
 utils::browseURL(gif_file)
 
